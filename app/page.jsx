4625bb7db5592c371b4e94a4c5036f759e59a1c2
tsurubaso <tsurubaso@hotmail.com>
2025-04-11 00:01:12 +0900

    j'avance le retapage du site j'ai mis le sub

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,18 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Imagier</h1>
-      <p className="mb-8 text-lg">Please log in or create an account</p>
-      
+      <h1 className="text-4xl font-bold mb-8">Welcome to Lunatech</h1>
+      <p className="mb-8 text-lg">Lets put it simply, we created a world and we  want to present the tech part</p>
+      <p className="mb-8 text-lg">All pictures are from pinterest, if you want to participate contact us</p>
+      <p className="mb-8 text-lg">We link also to the origin texts</p>
       <div className="space-x-4">
         <a 
-          href="/login" 
+          href="/welcome" 
           className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
         >
-          Login
+          Enter
         </a>
-        <a 
-          href="/signup" 
-          className="px-6 py-2 bg-gray-600 text-white rounded-md hover:bg-gray-700"
-        >
-          Sign Up
-        </a>
+
       </div>
     </div>
   )
